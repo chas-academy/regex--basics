bb6d5c14b714cd8a3301eca9f93c067d6a9651ca
Fredrik Karlsson <fredrikkarlsson320@gmail.com>
2026-02-12 11:35:34 +0100

    add strings

---
+++ main.js
@@ -0,0 +1,9 @@
+// hämta ut mailadresserna
+const emailString =
+  "Du kan nå mig på gus.davidson@chas1234.org, alternativt gus@chåsacademy.se";
+
+// matcha #_#
+const hashString = "     Hitta #_# mitt i meningen";
+
+// hämta ut alla vokaler och konsonanter och räkna dem
+const longString = "The quick brown fox jumps over the lazy dog.";
